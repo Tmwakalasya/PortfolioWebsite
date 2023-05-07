561200a108ccc115a9d8ddae4dd0dbbad94e0d23
Tmwakalasya <mwakalasyatuntu@gmail.com>
2023-05-07 17:34:43 -0400

    Add descriptions on the new columns

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 st.set_page_config(layout="wide")
-col1,col2 = st.columns(2)
+col1,col2= st.columns(2)
 with col1:
     st.image("images/picture.jpg")
 with col2:
@@ -10,3 +10,4 @@ at FIU pursuing a BS in Computer Science.
 I enjoy problem-solving and am also very intuitive I am always open to learning new things and 
 I enjoy exploring a variety of topics that interest Me such as Math, AI, Technology, and quantitative finance."""
     st.info(content)
+st.info("Below you can find some of the python projects. Feel free to contact me")
