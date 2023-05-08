b78cfea8616e73cd090a77a50e4e147f7d02544f
Tmwakalasya <mwakalasyatuntu@gmail.com>
2023-05-08 15:09:56 -0400

    render the app titles into two columns

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 st.set_page_config(layout="wide")
 col1,col2= st.columns(2)
 with col1:
@@ -11,3 +12,13 @@ I enjoy problem-solving and am also very intuitive I am always open to learning 
 I enjoy exploring a variety of topics that interest Me such as Math, AI, Technology, and quantitative finance."""
     st.info(content)
 st.info("Below you can find some of the python projects. Feel free to contact me")
+col4,col5 = st.columns(2)
+df = pandas.read_csv("venv/data.csv",sep=";")
+with col4:
+    for index,row in df.iterrows():
+        st.header(row["title"])
+with col5:
+    for index,row in df[10:].iterrows():
+        st.header(row["title"])
+
+
