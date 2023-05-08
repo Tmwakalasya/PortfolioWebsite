a64a9e2c90714e2289e42ea170ac22e0b914fe03
Tmwakalasya <mwakalasyatuntu@gmail.com>
2023-05-08 15:57:25 -0400

    Addd description,image and links to SRC

--- main.py
+++ main.py
@@ -12,13 +12,22 @@ I enjoy problem-solving and am also very intuitive I am always open to learning 
 I enjoy exploring a variety of topics that interest Me such as Math, AI, Technology, and quantitative finance."""
     st.info(content)
 st.info("Below you can find some of the python projects. Feel free to contact me")
-col4,col5 = st.columns(2)
+col4,empty_col,col5 = st.columns([1.5,0.5,1.5])
 df = pandas.read_csv("venv/data.csv",sep=";")
 with col4:
     for index,row in df.iterrows():
         st.header(row["title"])
+        st.write(row['description'])
+        st.image("images/" + row["image"])
+        st.write(f"[Source code]({row['url']})")
+
 with col5:
     for index,row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row['description'])
+        st.image("images/" + row["image"])
+        st.write(f"[Source code]({row['url']})")
+
+
 
 
