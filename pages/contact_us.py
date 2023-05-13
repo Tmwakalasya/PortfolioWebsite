3f7570be80c269b7266803abc1e3d22e1bbf6170
Tmwakalasya <mwakalasyatuntu@gmail.com>
2023-05-12 20:13:52 -0400

    add contact me page,add some text inputs email and message

---
+++ pages/contact_us.py
@@ -0,0 +1,6 @@
+import streamlit as st
+st.title("Contact me")
+with st.form("Contact me",clear_on_submit=True):
+    email = st.text_input("Please insert your email below")
+    message = st.text_area("Please leave me a message")
+    send_message = st.form_submit_button(label='send message',help='send note')
