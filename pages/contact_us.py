c402f9e7f0596056e948318e582af875f0428d7e
Tmwakalasya <mwakalasyatuntu@gmail.com>
2023-05-15 11:13:37 -0400

    send email

--- pages/contact_us.py
+++ pages/contact_us.py
@@ -3,4 +3,6 @@ st.title("Contact me")
 with st.form("Contact me",clear_on_submit=True):
     email = st.text_input("Please insert your email below")
     message = st.text_area("Please leave me a message")
-    send_message = st.form_submit_button(label='send message',help='send note')
+    send_button = st.form_submit_button(label='send message',help='send note')
+    # if send_button:
+
